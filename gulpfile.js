01b4f7b764f6127d77dfbd2a3d7382057c4c58dd
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-01-27 22:03:33 -0600

    minor code style changes

--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,17 @@ var siteDir = '_site';
 var siteFiles = siteDir + '/**';
 var cssFiles = '_css/**/*.?(s)css';
 
-gulp.task('jekyll', function() {
+gulp.task('jekyll', function () {
   var jekyll = spawn('bundle', ['exec', 'jekyll', 'build', '--watch', '--drafts']);
 
   jekyll.stdout.on('data', function (buffer) {
-    buffer.toString().trim().split(/\s*\n\s*/).forEach(function(message){
+    buffer.toString().trim().split(/\s*\n\s*/).forEach(function (message) {
       gutil.log('Jekyll: ' + message);
     });
   });
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   gulp.src(cssFiles)
     .pipe(scss().on('error', scss.logError))
     .pipe(concat('all.css'))
@@ -36,18 +36,18 @@ gulp.task('css', function() {
     .pipe(gulp.dest(siteDir + '/assets'));
 });
 
-gulp.task('site', function() {
+gulp.task('site', function () {
   gulp.src(siteFiles)
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(cssFiles, ['css']);
 
   gulp.watch(siteFiles, ['site']);
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   connect.server({
     port: 4000,
     root: siteDir,
